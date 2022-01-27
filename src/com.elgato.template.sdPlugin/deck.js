2cff57ce04ded841662d37710c499a156cb21fff
Zack Hoherchak <zachary.hoherchak@corsair.com>
2022-01-27 17:04:39 -0500

    socket updates

--- src/com.elgato.template.sdPlugin/deck.js
+++ src/com.elgato.template.sdPlugin/deck.js
@@ -1,19 +1,3 @@
-// Create a wrapper to allow passing JSON to the socket
-WebSocket.prototype.sendJSON = function (jsn, log) {
-    if (log) {
-        console.log('SendJSON', this, jsn);
-    }
-    // if (this.readyState) {
-    this.send(JSON.stringify(jsn));
-    // }
-};
-
-DestinationEnum = Object.freeze({
-    HARDWARE_AND_SOFTWARE: 0,
-    HARDWARE_ONLY: 1,
-    SOFTWARE_ONLY: 2
-});
-
 /**
  * StreamDeck object containing all required code to establish
  * communication with SD-Software and the Property Inspector
@@ -68,7 +52,7 @@ class StreamDeck {
                 uuid: this.uuid
             };
 
-            this.websocket.sendJSON(json);
+            this.websocket.send(JSON.stringify(json));
             $SD.uuid = this.uuid;
             $SD.actionInfo = this.actionInfo;
             $SD.applicationInfo = this.appInfo;
@@ -86,14 +70,13 @@ class StreamDeck {
         }
 
         this.websocket.onerror = (evt) => {
-            console.warn('WEBOCKET ERROR', evt, evt.data);
+            console.warn('WEBOCKET ERROR', evt, evt.data, SocketUtils.getErrorMessage(evt?.code));
         };
 
         this.websocket.onclose = (evt) => {
-            const reason = WEBSOCKETERROR(evt);
             console.warn(
                 '[STREAMDECK]***** WEBOCKET CLOSED **** reason:',
-                reason
+                SocketUtils.getErrorMessage(evt?.code)
             );
         };
 
@@ -233,7 +216,7 @@ const SDApi = {
          * all keys of the original JSON objects.
          */
         const pl = Object.assign({}, {event: fn, context: context}, payload);
-        $SD.websocket && $SD.websocket.sendJSON(pl);
+        $SD.websocket && $SD.websocket.send(JSON.stringify(pl));
 
         if (
             $SD.websocket &&
@@ -387,7 +370,7 @@ const SDApi = {
                 const payload = this[fn]();
                 if (typeof payload === 'object') {
                     Object.assign({event: fn, context: context}, payload);
-                    $SD.websocket && $SD.websocket.sendJSON(payload);
+                    $SD.websocket && $SD.websocket.send(JSON.stringify(payload));
                 }
             }
             console.log(this, fn, typeof this[fn], this[fn]());
@@ -424,45 +407,4 @@ const connectSocket = connectElgatoStreamDeckSocket;
 window.$SD = new StreamDeck();
 window.$SD.api = SDApi;
 
-function WEBSOCKETERROR(evt) {
-    // Websocket is closed
-    var reason = '';
-    if (evt.code === 1000) {
-        reason = 'Normal Closure. The purpose for which the connection was established has been fulfilled.';
-    } else if (evt.code === 1001) {
-        reason = 'Going Away. An endpoint is "going away", such as a server going down or a browser having navigated away from a page.';
-    } else if (evt.code === 1002) {
-        reason = 'Protocol error. An endpoint is terminating the connection due to a protocol error';
-    } else if (evt.code === 1003) {
-        reason = "Unsupported Data. An endpoint received a type of data it doesn't support.";
-    } else if (evt.code === 1004) {
-        reason = '--Reserved--. The specific meaning might be defined in the future.';
-    } else if (evt.code === 1005) {
-        reason = 'No Status. No status code was actually present.';
-    } else if (evt.code === 1006) {
-        reason = 'Abnormal Closure. The connection was closed abnormally, e.g., without sending or receiving a Close control frame';
-    } else if (evt.code === 1007) {
-        reason = 'Invalid frame payload data. The connection was closed, because the received data was not consistent with the type of the message (e.g., non-UTF-8 [http://tools.ietf.org/html/rfc3629]).';
-    } else if (evt.code === 1008) {
-        reason = 'Policy Violation. The connection was closed, because current message data "violates its policy". This reason is given either if there is no other suitable reason, or if there is a need to hide specific details about the policy.';
-    } else if (evt.code === 1009) {
-        reason = 'Message Too Big. Connection closed because the message is too big for it to process.';
-    } else if (evt.code === 1010) { // Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
-        reason = "Mandatory Ext. Connection is terminated the connection because the server didn't negotiate one or more extensions in the WebSocket handshake. <br /> Mandatory extensions were: " + evt.reason;
-    } else if (evt.code === 1011) {
-        reason = 'Internl Server Error. Connection closed because it encountered an unexpected condition that prevented it from fulfilling the request.';
-    } else if (evt.code === 1015) {
-        reason = "TLS Handshake. The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified).";
-    } else {
-        reason = 'Unknown reason';
-    }
 
-    return reason;
-}
-
-const SOCKETERRORS = {
-    '0': 'The connection has not yet been established',
-    '1': 'The connection is established and communication is possible',
-    '2': 'The connection is going through the closing handshake',
-    '3': 'The connection has been closed or could not be opened'
-};
---
+++ src/com.elgato.template.sdPlugin/libs/destination-enum.js
@@ -0,0 +1,5 @@
+var DestinationEnum = Object.freeze({
+    HARDWARE_AND_SOFTWARE: 0,
+    HARDWARE_ONLY: 1,
+    SOFTWARE_ONLY: 2
+});
