1b994c0c8ad479f740ba2efade978c16c0042422
Zack Hoherchak <zachary.hoherchak@corsair.com>
2022-02-04 14:45:55 -0500

    fixed action name bug

--- src/com.elgato.template.sdPlugin/app.js
+++ src/com.elgato.template.sdPlugin/app.js
@@ -6,7 +6,7 @@
  * The first event fired when Stream Deck starts
  */
 StreamDeck.registerConnected(({actionInfo, appInfo, connection, messageType, port, uuid}) => {
-    const templateAction = new Action('com.elgato.template.actions', StreamDeck.events);
+    const templateAction = new Action('com.elgato.template.action', StreamDeck.events);
 
     templateAction.registerKeyUp((jsn) => {
         const {action, context, device, event, payload} = jsn;
