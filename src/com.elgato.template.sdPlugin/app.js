9654f5190f303f347f46702566f5e48b0a7bcfac
Zack Hoherchak <zachary.hoherchak@corsair.com>
2022-02-11 23:56:37 -0500

    rename action

--- src/com.elgato.template.sdPlugin/app.js
+++ src/com.elgato.template.sdPlugin/app.js
@@ -6,14 +6,14 @@ const streamDeck = new StreamDeck();
 /**
  * The first event fired when Stream Deck starts
  */
-streamDeck.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) => {
-	const templateAction = new Action('com.elgato.template.action');
+streamDeck.onConnected(({actionInfo, appInfo, connection, messageType, port, uuid}) => {
+    const myAction = new Action('com.elgato.template.action');
 
-	templateAction.onKeyUp(({ action, context, device, event, payload }) => {
-		streamDeck.openUrl('https://developer.elgato.com/documentation/stream-deck/sdk/overview/');
+    myAction.onKeyUp(({action, context, device, event, payload}) => {
+        streamDeck.openUrl('https://developer.elgato.com/documentation/stream-deck/sdk/overview/');
 
-		console.log('Your code goes here!');
+        console.log('Your code goes here!');
 
-		streamDeck.showOk(context);
-	});
+        streamDeck.showOk(context);
+    });
 });
